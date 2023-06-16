0a7b047e0fa4f5a208ea85bd909f4f5f44baf844
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-16 23:47:41 +0530

    Sample commit

--- app.js
+++ app.js
@@ -13,7 +13,18 @@ const server = http.createServer((req, res)=>{
     }
 
     if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
+        
         res.statusCode = 302;
         res.setHeader('Location','/');
         return res.end();
