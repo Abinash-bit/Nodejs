2ee196766df56b2bea2745b08fdec78b5f9f1558
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-19 00:09:55 +0530

    Sample commit

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 
 app.use('/add-product' , (req, res, next) => {
-    console.log('In another middleware');
+    
     res.send('<h1>Hello from Add Products</h1>');
 });
 
 app.use('/' , (req, res, next) => {
-    console.log('In another middleware');
+    
     res.send('<h1>Hello from Express!</h1>');
 });
 
