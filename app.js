03ce247a563fa5df3493dad030a152658e63db38
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-18 23:44:48 +0530

    Sample commit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
 });
 const server = http.createServer(app);
 
