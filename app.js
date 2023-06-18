424b6426e4642d133090a6467f9899d6e640ede2
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-18 12:03:27 +0530

    Sample commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(3000);
