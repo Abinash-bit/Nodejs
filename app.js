7e3930f94ec07723697829d58316090c4aa2151b
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-19 17:25:50 +0530

    Sample commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use('/add-product' , (req, res, next) => {
     
-    res.send('<form><input type="text" name="title"><button type="submit">add products</button></form>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add products</button></form>');
 });
 
 app.use('/' , (req, res, next) => {
