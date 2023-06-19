269f016dbf7963a71051ddfc640ef08f74ffc40e
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-19 22:57:58 +0530

    Sample commit

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-application.use('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
