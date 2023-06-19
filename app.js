2c311e453d3e2c1757562dad2aeb25cb67c2740a
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-19 21:08:51 +0530

    Sample commit

--- app.js
+++ app.js
@@ -10,15 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 
-app.use('/add-product' , (req, res, next) => {
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add products</button></form>');
-});
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
 
 app.use('/' , (req, res, next) => {
     
