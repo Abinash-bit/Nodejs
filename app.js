646b0312bb7f60b39f7a8d9834bed0a694025ef1
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-06-19 23:27:03 +0530

    Sample commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product' , (req, res, next) => {
+router.get('/add-product' , (req, res, next) => {
     
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add products</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add products</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
